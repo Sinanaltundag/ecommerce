5efd55f74aa45bb46d823c5a5f5034383e75289f
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-06-04 21:18:53 +0300

    Carousel added, fake data added

---
+++ src/helpers/data.jsx
@@ -0,0 +1,81 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://pixabay.com/get/g85a4c53df05fb5a982604eb477ff24dad638bd037a654856c53ad1f52ca7debe4c85b1e34cd7c05b8d0af9e99cea8cad45a058209d83438a6db90849ed14ca6d_1280.jpg",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+      pixabay:"https://pixabay.com/api/?key=26174232-f956318e8a670396f67464227&id=1274056"
+    },
+    {
+      id: 2,
+      img: "https://pixabay.com/get/gb9eca585e4e605d27210383dde22d7e078464ca28a56dbeeac2ef891690a5f3582b1b06c7ccf2e3376480275295d6eaeb72d480fdca94794da578156ff67e91e_1280.jpg",
+      title: "AUTUMN KIDS",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+      pixabay:"https://pixabay.com/api/?key=26174232-f956318e8a670396f67464227&id=3363362"
+    },
+    {
+      id: 3,
+      img: "https://pixabay.com/get/g174d7990fda5619dbcaf1258b2697e046bde24bf36ed48a3f07c268a0034b50ecdba34b443dc55d5dce7b3999f854ecea92ed2771cbff9fca1bde39fce8bf3d4_1280.jpg",
+      title: "WINTER COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+      pixabay:"https://pixabay.com/api/?key=26174232-f956318e8a670396f67464227&id=1274361"
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://pixabay.com/get/g90b4b368b9923671e52e45835c6af31fecc5c73c7b51e0b83e097b0d41ade12245beaf15f122b5b7d1f633cea7ce7e8128dd94b240a35189bed454fbc5d6af78_640.jpg",
+      pixabay:"https://pixabay.com/api/?key=26174232-f956318e8a670396f67464227&id=2911332"
+    },
+    {
+      id:2,
+      img:"https://pixabay.com/get/g16a3d00c28b5af5a88a95961e2f621a8b00ada55ff2d1ad9d423ddc7890e6ecd2b49b3940afc06fe4aaef309570804211387e11f4d1c22a4419a494b5ba12a4a_640.jpg",
+      pixabay:"https://pixabay.com/api/?key=26174232-f956318e8a670396f67464227&id=2619784"
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://pixabay.com/get/g177ad7683fcfe3df8bde5f0aa5be4beff16bbf2c6a21f5b529007f8fca651268127f2f71b526e5722e0617f06019e326b3163e8d0286ce13a3b7e0b3e28dc08d_640.jpg",
+    },
+    {
+      id:7,
+      img:"https://pixabay.com/get/gd31e1813fb32d2111aeec82c200df32cdab952aaf66c7f1cf5921bdb04091af5a80a22516896a245a48d2be7688d2f2c85de099e9c5ede633684676318431003_640.jpg",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
