23726eb9d6ac67e13cff29d57874b5ad8ddf3a17
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-06-06 00:33:40 +0300

    change some links broken

--- src/helpers/data.jsx
+++ src/helpers/data.jsx
@@ -1,7 +1,7 @@
 export const sliderItems = [
     {
       id: 1,
-      img: "https://pixabay.com/get/g85a4c53df05fb5a982604eb477ff24dad638bd037a654856c53ad1f52ca7debe4c85b1e34cd7c05b8d0af9e99cea8cad45a058209d83438a6db90849ed14ca6d_1280.jpg",
+      img: "https://cdn.pixabay.com/photo/2016/03/23/04/01/woman-1274056_960_720.jpg",
       title: "SUMMER SALE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "f5fafd",
@@ -9,7 +9,7 @@ export const sliderItems = [
     },
     {
       id: 2,
-      img: "https://pixabay.com/get/gb9eca585e4e605d27210383dde22d7e078464ca28a56dbeeac2ef891690a5f3582b1b06c7ccf2e3376480275295d6eaeb72d480fdca94794da578156ff67e91e_1280.jpg",
+      img: "https://cdn.pixabay.com/photo/2018/04/30/17/26/autumn-3363362_960_720.jpg",
       title: "AUTUMN KIDS",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fcf1ed",
@@ -17,7 +17,7 @@ export const sliderItems = [
     },
     {
       id: 3,
-      img: "https://pixabay.com/get/g174d7990fda5619dbcaf1258b2697e046bde24bf36ed48a3f07c268a0034b50ecdba34b443dc55d5dce7b3999f854ecea92ed2771cbff9fca1bde39fce8bf3d4_1280.jpg",
+      img: "https://cdn.pixabay.com/photo/2016/03/23/08/34/woman-1274361_960_720.jpg",
       title: "WINTER COLLECTION",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fbf0f4",
@@ -46,12 +46,12 @@ export const sliderItems = [
   export const popularProducts = [
     {
       id:1,
-      img:"https://pixabay.com/get/g90b4b368b9923671e52e45835c6af31fecc5c73c7b51e0b83e097b0d41ade12245beaf15f122b5b7d1f633cea7ce7e8128dd94b240a35189bed454fbc5d6af78_640.jpg",
+      img:"https://cdn.pixabay.com/photo/2017/11/02/14/27/model-2911332_960_720.jpg",
       pixabay:"https://pixabay.com/api/?key=26174232-f956318e8a670396f67464227&id=2911332"
     },
     {
       id:2,
-      img:"https://pixabay.com/get/g16a3d00c28b5af5a88a95961e2f621a8b00ada55ff2d1ad9d423ddc7890e6ecd2b49b3940afc06fe4aaef309570804211387e11f4d1c22a4419a494b5ba12a4a_640.jpg",
+      img:"https://cdn.pixabay.com/photo/2017/08/10/08/00/suit-2619784_960_720.jpg",
       pixabay:"https://pixabay.com/api/?key=26174232-f956318e8a670396f67464227&id=2619784"
     },
     {
@@ -68,11 +68,13 @@ export const sliderItems = [
     },
     {
       id:6,
-      img:"https://pixabay.com/get/g177ad7683fcfe3df8bde5f0aa5be4beff16bbf2c6a21f5b529007f8fca651268127f2f71b526e5722e0617f06019e326b3163e8d0286ce13a3b7e0b3e28dc08d_640.jpg",
+      img:"https://cdn.pixabay.com/photo/2017/08/01/11/48/woman-2564660_960_720.jpg",
+      pixabay:"https://pixabay.com/api/?key=26174232-f956318e8a670396f67464227&id=2564660"
     },
     {
       id:7,
-      img:"https://pixabay.com/get/gd31e1813fb32d2111aeec82c200df32cdab952aaf66c7f1cf5921bdb04091af5a80a22516896a245a48d2be7688d2f2c85de099e9c5ede633684676318431003_640.jpg",
+      img:"https://cdn.pixabay.com/photo/2020/03/02/12/13/accessories-4895580_960_720.jpg",
+        pixabay:"https://pixabay.com/api/?key=26174232-f956318e8a670396f67464227&id=4895580",
     },
     {
       id:8,
