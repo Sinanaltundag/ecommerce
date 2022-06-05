78d4156f38d445b76022b0bb8c1e05a73884c3cf
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-06-06 00:32:50 +0300

    add product component

---
+++ src/components/Product.jsx
@@ -0,0 +1,87 @@
+import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from "@mui/icons-material"
+import styled from "styled-components"
+
+const Info= styled.div`
+opacity: 0;
+width: 100%;
+height: 100%;
+position: absolute;
+top: 0;
+left: 0;
+background-color: rgba(0, 0, 0, 0.2);
+z-index: 3;
+display: flex;
+align-items: center;
+justify-content: center;
+transition: all 0.5s ease-in-out;
+
+`
+
+const Container= styled.div`
+flex: 1;
+margin: 0.3rem;
+min-width: 280px;
+height: 350px;
+display: flex;
+align-items: center;
+justify-content: center;
+background-color: #faf8f6;
+position: relative;
+&:hover ${Info} {
+opacity: 1;
+}
+`
+const Circle= styled.div`
+width: 200px;
+height: 200px;
+border-radius: 50%;
+background-color: #faf8f6;
+position: absolute;
+`
+const Image= styled.img`
+height: 75%;
+max-width: 280px;
+object-fit: cover;
+z-index: 2;
+`
+const Icon= styled.div`
+width:2.5rem;
+height:2.5rem;
+border-radius: 50%;
+background-color: #faf8f6;
+display: flex;
+align-items: center;
+justify-content: center;
+margin: 0.3rem;
+transition: all 0.3s ease-in-out;
+cursor: pointer;
+&:hover{
+    background-color: #008080;
+    color: white;
+    transform: scale(1.1);
+}
+`
+
+const Product = ({img}) => {
+  return (
+    <div>
+<Container>
+    <Circle/>
+    <Image src={img} />
+    <Info>
+        <Icon>
+            <ShoppingCartOutlined/>
+        </Icon>
+        <Icon>
+            <SearchOutlined/>
+        </Icon>
+        <Icon>
+            <FavoriteBorderOutlined/>
+        </Icon>
+    </Info>
+</Container>
+    </div>
+  )
+}
+
+export default Product
---
+++ src/components/Products.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+import { popularProducts } from "../helpers/data"
+import Product from "./Product"
+
+
+const Container = styled.div`
+padding: 1.2rem;
+display: flex;
+flex-wrap: wrap;
+justify-content: space-between;
+`
+
+const Products = () => {
+  return (
+    <Container>
+{popularProducts.map(product => (
+    <Product key={product.id} {...product} />
+))}
+    </Container>
+  )
+}
+
+export default Products
