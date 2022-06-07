7153751ed0ef54b2a9594ac2a54ef92d9e922a54
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-06-08 01:11:27 +0300

    react router added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Announcement from './components/Announcement';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Newsletter from './components/Newsletter';
+import Products from './components/Products';
+import Cart from './pages/Cart';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Product from './pages/Product';
+import ProductList from './pages/ProductList';
+import Register from './pages/Register';
 
 function App() {
+  const location = useLocation();
+  const disabled = (location.pathname=== "/login" || location.pathname === "/register") 
+  const home = (location.pathname=== "/login")
+  
   return (
-    <Home/>
+    <div className="App">
+    {disabled||<Announcement/>}
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/login" element={<Login disabled={disabled} />} />
+      <Route path="/register" element={<Register disabled={disabled}/>} />
+      <Route path="/product" element={<Product disabled={disabled}/>} />
+      <Route path="/productlist" element={<ProductList products={<Products/>} />} />
+      <Route path="/cart" element={<Cart/>} />
+    </Routes>
+
+    {home&&
+      <>
+
+    <Products/>
+    <Newsletter/>
+    <Footer/>
+      </>}
+    </div>
   );
 }
 
